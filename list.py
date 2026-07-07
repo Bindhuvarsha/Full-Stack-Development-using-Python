23a0bc9102dd347ade515593fe9895817028cada
Bindhu Shree.K.R <bindhushreebindhushree28@gmail.com>
2026-07-07 15:26:08 +0530

    main

---
+++ list.py
@@ -0,0 +1,15 @@
+# student1 = "ram"
+# student2 = "raju"
+# student3 = "bheem"
+# print(student1)
+
+
+
+student =[
+    "ram",
+    "raju",
+    "rana"
+]
+student.append("bheem")
+student.remove("ram")
+print(student)
