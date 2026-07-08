f2fcd18c8805ac297f2067370542eb0032552597
Bindhu Shree.K.R <bindhushreebindhushree28@gmail.com>
2026-07-08 10:58:48 +0530

    Day3

--- day3.py
+++ day3.py
@@ -17,6 +17,70 @@
 
 
 
-x = (1,2,3,2,1,1,1)
-print(x.count(3))
-print(x.count(2))
+# x = (1,2,3,2,1,1,1)
+# print(x.count(3))
+# print(x.count(2))
+
+
+# x = ("ram","ham","sam","jam","ram","ram")
+# print(x.count("ram"))
+
+
+# x = ("ram","ham","sam","jam","ram","ram")
+# print(x.index("sam"))
+
+
+# ##############tuple slicing################    Tuple slicing in  allows you to extract a subset of elements from a tuple using start, stop, and step indices.
+# num = (10,20,30,40,50)
+# print(num[1:4])
+
+
+
+
+##################sets######################### set is a  collection of:remove duplicate values : stores unique values :no indexing :
+# x ={1,2,3,2,1,1,1}
+# print(x)
+# data = {1,2,3}
+# data.add(4)
+# print(data)
+
+
+##############union,intersection#################
+# a = {1,2,3}
+# b = {3,4,5}
+# print(a|b)
+
+
+# a = {1,2,3}
+# b = {3,4,5}
+# print(a & b)
+
+
+###############fuction#######################
+# def greeting():
+#     print("hello students")
+# greeting()
+
+def add():
+     return 10 + 20 
+result = add()
+print(result)
+
+
+def sub() :
+     return 10 -20
+result = sub()
+print(result)
+
+def mul() :
+     return 10 * 2
+result = mul()
+print(result)
+
+
+def div() :
+     return 10 / 5
+result = div()
+print(result)
+
+
